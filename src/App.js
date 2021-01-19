868b5c7b8203964c790de81bf1be3b7ffba5a318
unknown <juliapohlmann@hotmail.com>
2021-01-19 18:30:13 -0300

    +texto highlighted acrescentando a marcacao quando alguma estiver selecionada

--- src/App.js
+++ src/App.js
@@ -3,17 +3,56 @@ import React, { useState } from 'react';
 import './global.css';
 
 function App() {
-  const [ marker, setMarker ] = useState([]);
+  const [ marker, setMarker ] = useState('');
+  const [ person, setPerson ] = useState([]);
+  const [ action, setAction ] = useState([]);
+  const [ value,  setValue  ] = useState([]);
+  const [ object, setObject ] = useState([]);
 
-  function selectMarker(markerValue){
-    setMarker(markerValue);
+  document.onmouseup = selectText;
+
+  function selectMarker(markerValue) {
+    if(marker === markerValue) setMarker('');
+    else setMarker(markerValue);
+  }
+
+  function selectText(e) {
+    let selectedText =  document.getSelection();
+    if( selectedText.baseNode.parentElement.id === 'documentText' &&
+        selectedText.toString().length > 0){    
+      newTextMarked(selectedText);
+    }
+  }
+
+  function newTextMarked(selection) {
+    let obj = {
+      text: selection.toString(),
+      docStart: selection.baseOffset,
+      docEnd: selection.focusOffset
+    }
+    switch (marker) {
+      case 'pessoa':
+        setPerson([...person, obj]);
+        break;
+      case 'ação':
+        setAction([...action, obj]);
+        break;
+      case 'valor':
+        setValue([...value, obj]);
+        break;
+      case 'objeto':
+        setObject([...object, obj]);
+        break;
+      default:
+        break;
+    }
   }
 
   return (
     <div className="textAnalyzer">
       <div>
         <h3>Documento</h3>
-        <span>O Sr. e a Sra. Dursley, da rua dos Alfeneiros, no 4, se orgulhavam de dizer que eram
+        <span id="documentText">O Sr. e a Sra. Dursley, da rua dos Alfeneiros, no 4, se orgulhavam de dizer que eram
               perfeitamente normais, muito bem, obrigado. Eram as últimas pessoas no mundo que se
               esperaria que se metessem em alguma coisa estranha ou misteriosa, porque simplesmente não
               compactuavam com esse tipo de bobagem. 
@@ -37,11 +76,43 @@ function App() {
       <div>
         <h3>Marcadores</h3>
         <p>Marcador selecionado: {marker}</p>
+
         <div className="markers">
           <button id="personMarker" value="pessoa" onClick={e => selectMarker(e.target.value)}>Pessoa</button>
+          <span>
+            {person.map((person, index) =>(
+              <p key={index}>
+                {index}: {person.text}
+              </p>
+            ))}
+          </span>
+
           <button id="actionMarker" value="ação"   onClick={e => selectMarker(e.target.value)}>Ação</button>
+          <span>
+            {action.map((action, index) =>(
+              <p key={index}>
+                {index}:{action.text}
+              </p>
+            ))}
+          </span>
+
           <button id="valueMarker"  value="valor"  onClick={e => selectMarker(e.target.value)}>Valor</button>
+          <span>
+            {value.map((value, index) =>(
+              <p key={index}>
+                {index}:{value.text}
+              </p>
+            ))}
+          </span>
+
           <button id="objectMarker" value="objeto" onClick={e => selectMarker(e.target.value)}>Objeto</button>
+          <span>
+            {object.map((object, index) =>(
+              <p key={index}>
+                {index}:{object.text}
+              </p>
+            ))}
+          </span>
         </div>
       </div>
     </div>
