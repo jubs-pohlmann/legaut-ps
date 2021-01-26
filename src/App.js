b8ca1079c8b5d6af8197bf04fae225000fa72a70
unknown <juliapohlmann@hotmail.com>
2021-01-26 13:46:54 -0300

    (bug) texto highlight ao selecionar string+tag

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [ action, setAction ] = useState([]);
   const [ value,  setValue  ] = useState([]);
   const [ object, setObject ] = useState([]);
+  const [ documentText, setDocumentText ] = useState('O Sr. e a Sra. Dursley, da rua dos Alfeneiros, no 4, se orgulhavam de dizer que eram perfeitamente normais, muito bem, obrigado. Eram as últimas pessoas no mundo que se esperaria que se metessem em alguma coisa estranha ou misteriosa, porque simplesmente não compactuavam com esse tipo de bobagem.  O Sr. Dursley era diretor de uma firma chamada Grunnings, que fazia perfurações. Era um homem alto e corpulento quase sem pescoço, embora tivesse enormes bigodes. A Sra. Dursley era magra e loura e tinha um pescoço quase duas vezes mais comprido que o normal, o que era muito útil porque ela passava grande parte do tempo espichando-o por cima da cerca do jardim para espiar os vizinhos. Os Dursley tinham um filhinho chamado Dudley, o Duda, e em sua opinião não havia garoto melhor em nenhum lugar do mundo. Os Dursley tinham tudo que queriam, mas tinham também um segredo, e seu maior receio era que alguém o descobrisse. Achavam que não iriam aguentar se alguém descobrisse a existência dos Potter. A Sra. Potter era irmã da Sra. Dursley, mas não se viam havia muitos anos; na realidade a Sra. Dursley fingia que não tinha irmã, porque esta e o marido imprestável eram o que havia de menos parecido possível com os Dursley. Eles estremeciam só de pensar o que os vizinhos iriam dizer se os Potter aparecessem na rua. Os Dursley sabiam que os Potter tinham um filhinho, também, mas nunca o tinham visto. O garoto era mais uma razão para manter os Potter a distância; eles não queriam que Duda se misturasse com uma criança daquelas.');
 
   document.onmouseup = selectText;
 
@@ -18,10 +19,20 @@ function App() {
 
   function selectText(e) {
     let selectedText =  document.getSelection();
-    if( selectedText.baseNode.parentElement.id === 'documentText' &&
-        selectedText.toString().length > 0){    
+    if( selectedText.baseNode &&
+        selectedText.baseNode.parentElement.id === 'documentText' &&
+        selectedText.toString().length > 0 &&
+        marker != ''){    
       newTextMarked(selectedText);
     }
+  }
+
+  function highlight(selection){
+    console.log(selection);
+    let newText = documentText.substring(0, selection.docStart) + `<span class="${marker}Marker">` + documentText.substring(selection.docStart, selection.docEnd) + "</span>" + documentText.substring(selection.docEnd, documentText.length);
+    setDocumentText(newText);
+    document.getElementById('documentText').innerHTML = documentText;
+    console.log(document.getElementById('documentText'));
   }
 
   function newTextMarked(selection) {
@@ -30,6 +41,7 @@ function App() {
       docStart: selection.baseOffset,
       docEnd: selection.focusOffset
     }
+    console.log(obj, documentText);
     switch (marker) {
       case 'pessoa':
         setPerson([...person, obj]);
@@ -46,6 +58,7 @@ function App() {
       default:
         break;
     }
+    highlight(obj);
   }
 
   return (
@@ -78,7 +91,7 @@ function App() {
         <p>Marcador selecionado: {marker}</p>
 
         <div className="markers">
-          <button id="personMarker" value="pessoa" onClick={e => selectMarker(e.target.value)}>Pessoa</button>
+          <button className="personMarker" value="person" onClick={e => selectMarker(e.target.value)}>Pessoa</button>
           <span>
             {person.map((person, index) =>(
               <p key={index}>
@@ -87,29 +100,29 @@ function App() {
             ))}
           </span>
 
-          <button id="actionMarker" value="ação"   onClick={e => selectMarker(e.target.value)}>Ação</button>
+          <button className="actionMarker" value="action"   onClick={e => selectMarker(e.target.value)}>Ação</button>
           <span>
             {action.map((action, index) =>(
               <p key={index}>
-                {index}:{action.text}
+                {index}: {action.text}
               </p>
             ))}
           </span>
 
-          <button id="valueMarker"  value="valor"  onClick={e => selectMarker(e.target.value)}>Valor</button>
+          <button className="valueMarker"  value="value"  onClick={e => selectMarker(e.target.value)}>Valor</button>
           <span>
             {value.map((value, index) =>(
               <p key={index}>
-                {index}:{value.text}
+                {index}: {value.text}
               </p>
             ))}
           </span>
 
-          <button id="objectMarker" value="objeto" onClick={e => selectMarker(e.target.value)}>Objeto</button>
+          <button className="objectMarker" value="object" onClick={e => selectMarker(e.target.value)}>Objeto</button>
           <span>
             {object.map((object, index) =>(
               <p key={index}>
-                {index}:{object.text}
+                {index}: {object.text}
               </p>
             ))}
           </span>
