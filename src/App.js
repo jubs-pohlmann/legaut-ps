4e583db61459840fc7b33715b9e6e33f9a182746
unknown <juliapohlmann@hotmail.com>
2021-01-26 22:56:26 -0300

    *marcacao das selecoes no texto fix | (bug) selecionar de tras pra frente gera uma marcacao incorreta

--- src/App.js
+++ src/App.js
@@ -3,87 +3,112 @@ import React, { useState } from 'react';
 import './global.css';
 
 function App() {
+  const documentText = 'O Sr. e a Sra. Dursley, da rua dos Alfeneiros, no 4, se orgulhavam de dizer que eram perfeitamente normais, muito bem, obrigado. Eram as últimas pessoas no mundo que se esperaria que se metessem em alguma coisa estranha ou misteriosa, porque simplesmente não compactuavam com esse tipo de bobagem.  O Sr. Dursley era diretor de uma firma chamada Grunnings, que fazia perfurações. Era um homem alto e corpulento quase sem pescoço, embora tivesse enormes bigodes. A Sra. Dursley era magra e loura e tinha um pescoço quase duas vezes mais comprido que o normal, o que era muito útil porque ela passava grande parte do tempo espichando-o por cima da cerca do jardim para espiar os vizinhos. Os Dursley tinham um filhinho chamado Dudley, o Duda, e em sua opinião não havia garoto melhor em nenhum lugar do mundo. Os Dursley tinham tudo que queriam, mas tinham também um segredo, e seu maior receio era que alguém o descobrisse. Achavam que não iriam aguentar se alguém descobrisse a existência dos Potter. A Sra. Potter era irmã da Sra. Dursley, mas não se viam havia muitos anos; na realidade a Sra. Dursley fingia que não tinha irmã, porque esta e o marido imprestável eram o que havia de menos parecido possível com os Dursley. Eles estremeciam só de pensar o que os vizinhos iriam dizer se os Potter aparecessem na rua. Os Dursley sabiam que os Potter tinham um filhinho, também, mas nunca o tinham visto. O garoto era mais uma razão para manter os Potter a distância; eles não queriam que Duda se misturasse com uma criança daquelas.';
+  
   const [ marker, setMarker ] = useState('');
   const [ person, setPerson ] = useState([]);
   const [ action, setAction ] = useState([]);
   const [ value,  setValue  ] = useState([]);
   const [ object, setObject ] = useState([]);
-  const [ documentText, setDocumentText ] = useState('O Sr. e a Sra. Dursley, da rua dos Alfeneiros, no 4, se orgulhavam de dizer que eram perfeitamente normais, muito bem, obrigado. Eram as últimas pessoas no mundo que se esperaria que se metessem em alguma coisa estranha ou misteriosa, porque simplesmente não compactuavam com esse tipo de bobagem.  O Sr. Dursley era diretor de uma firma chamada Grunnings, que fazia perfurações. Era um homem alto e corpulento quase sem pescoço, embora tivesse enormes bigodes. A Sra. Dursley era magra e loura e tinha um pescoço quase duas vezes mais comprido que o normal, o que era muito útil porque ela passava grande parte do tempo espichando-o por cima da cerca do jardim para espiar os vizinhos. Os Dursley tinham um filhinho chamado Dudley, o Duda, e em sua opinião não havia garoto melhor em nenhum lugar do mundo. Os Dursley tinham tudo que queriam, mas tinham também um segredo, e seu maior receio era que alguém o descobrisse. Achavam que não iriam aguentar se alguém descobrisse a existência dos Potter. A Sra. Potter era irmã da Sra. Dursley, mas não se viam havia muitos anos; na realidade a Sra. Dursley fingia que não tinha irmã, porque esta e o marido imprestável eram o que havia de menos parecido possível com os Dursley. Eles estremeciam só de pensar o que os vizinhos iriam dizer se os Potter aparecessem na rua. Os Dursley sabiam que os Potter tinham um filhinho, também, mas nunca o tinham visto. O garoto era mais uma razão para manter os Potter a distância; eles não queriam que Duda se misturasse com uma criança daquelas.');
-
+  const [ markedDocument, setMarkedDocument ] = useState(splitText( documentText ));
+  
   document.onmouseup = selectText;
-
-  function selectMarker(markerValue) {
-    if(marker === markerValue) setMarker('');
-    else setMarker(markerValue);
+  
+  function selectMarker( markerValue ) {
+    if( marker === markerValue ) setMarker( '' );
+    else setMarker( markerValue );
   }
 
-  function selectText(e) {
+  function splitText( text ){
+    let wordArray =  text.split(' ');
+    let charIndex = 0;
+    wordArray.forEach( ( word, index ) => {
+      let obj = {
+        text: `${word} `,
+        marker: '',
+        indexStart: charIndex,
+        color: ''
+      }
+      charIndex += word.length + 1; 
+      wordArray[index] = obj;
+    });
+    return wordArray;
+  }
+
+  function selectText( e ) {
     let selectedText =  document.getSelection();
     if( selectedText.baseNode &&
-        selectedText.baseNode.parentElement.id === 'documentText' &&
+        selectedText.baseNode.parentElement.className === 'markedDocument' &&
         selectedText.toString().length > 0 &&
         marker != ''){    
       newTextMarked(selectedText);
     }
   }
 
-  function highlight(selection){
-    console.log(selection);
-    let newText = documentText.substring(0, selection.docStart) + `<span class="${marker}Marker">` + documentText.substring(selection.docStart, selection.docEnd) + "</span>" + documentText.substring(selection.docEnd, documentText.length);
-    setDocumentText(newText);
-    document.getElementById('documentText').innerHTML = documentText;
-    console.log(document.getElementById('documentText'));
-  }
-
   function newTextMarked(selection) {
+    let selectedText = selection.toString();
+    let startIndex = parseInt(selection.baseNode.parentElement.id);
+    let endIndex = startIndex + selectedText.split(' ').length - 1; 
     let obj = {
-      text: selection.toString(),
-      docStart: selection.baseOffset,
-      docEnd: selection.focusOffset
+      text: selectedText
     }
-    console.log(obj, documentText);
+
+    // if( selection.focusOffset < selection.baseOffset ){ //caso a selecao ocorra de tras pra frente
+    //   endIndex = startIndex;
+    //   startIndex = endIndex - selectedText.split(' ').length;
+    // }
+    
+    console.log(
+      selection, 
+      selectedText, 
+      startIndex, endIndex,
+      
+      );
+
+    for (let index = startIndex; index <= endIndex; index++) {
+      let copy = markedDocument;
+      if(markedDocument[index].marker == marker){ //se marcado remover da pilha // tbm remover caso marcado com outra
+        copy[index].marker = '';
+        copy[index].color = '';
+        setMarkedDocument(copy);
+      } else {
+        copy[index].marker = marker;
+        switch (marker) {
+          case 'person':
+            copy[index].color = 'yellow'; break;
+          case 'action':
+            copy[index].color = 'pink'; break;
+          case 'value':
+            copy[index].color = 'greenyellow'; break;
+          case 'object':
+            copy[index].color = 'lightblue'; break;
+        }
+        setMarkedDocument(copy);
+      }
+    }
+
     switch (marker) {
-      case 'pessoa':
-        setPerson([...person, obj]);
-        break;
-      case 'ação':
-        setAction([...action, obj]);
-        break;
-      case 'valor':
-        setValue([...value, obj]);
-        break;
-      case 'objeto':
-        setObject([...object, obj]);
-        break;
-      default:
-        break;
+      case 'person':
+        setPerson([...person, obj]); break;
+      case 'action':
+        setAction([...action, obj]); break;
+      case 'value':
+        setValue([...value, obj]); break;
+      case 'object':
+        setObject([...object, obj]); break;
     }
-    highlight(obj);
   }
 
   return (
     <div className="textAnalyzer">
       <div>
         <h3>Documento</h3>
-        <span id="documentText">O Sr. e a Sra. Dursley, da rua dos Alfeneiros, no 4, se orgulhavam de dizer que eram
-              perfeitamente normais, muito bem, obrigado. Eram as últimas pessoas no mundo que se
-              esperaria que se metessem em alguma coisa estranha ou misteriosa, porque simplesmente não
-              compactuavam com esse tipo de bobagem. 
-              O Sr. Dursley era diretor de uma firma chamada Grunnings, que fazia perfurações. Era um
-              homem alto e corpulento quase sem pescoço, embora tivesse enormes bigodes. A Sra. Dursley
-              era magra e loura e tinha um pescoço quase duas vezes mais comprido que o normal, o que era
-              muito útil porque ela passava grande parte do tempo espichando-o por cima da cerca do
-              jardim para espiar os vizinhos. Os Dursley tinham um filhinho chamado Dudley, o Duda, e em
-              sua opinião não havia garoto melhor em nenhum lugar do mundo.
-              Os Dursley tinham tudo que queriam, mas tinham também um segredo, e seu maior receio
-              era que alguém o descobrisse. Achavam que não iriam aguentar se alguém descobrisse a
-              existência dos Potter. A Sra. Potter era irmã da Sra. Dursley, mas não se viam havia muitos
-              anos; na realidade a Sra. Dursley fingia que não tinha irmã, porque esta e o marido
-              imprestável eram o que havia de menos parecido possível com os Dursley. Eles estremeciam
-              só de pensar o que os vizinhos iriam dizer se os Potter aparecessem na rua. Os Dursley sabiam
-              que os Potter tinham um filhinho, também, mas nunca o tinham visto. O garoto era mais uma
-              razão para manter os Potter a distância; eles não queriam que Duda se misturasse com uma
-              criança daquelas.
+        <span id="documentText">
+          {markedDocument.map((word, index) =>(
+              <span className="markedDocument" id={index} style={{background: word.color}} key={index}>
+                {word.text}
+              </span>
+            ))}
         </span>
       </div>
       <div>
@@ -95,7 +120,7 @@ function App() {
           <span>
             {person.map((person, index) =>(
               <p key={index}>
-                {index}: {person.text}
+                ({index}) {person.text}
               </p>
             ))}
           </span>
@@ -104,7 +129,7 @@ function App() {
           <span>
             {action.map((action, index) =>(
               <p key={index}>
-                {index}: {action.text}
+                ({index}) {action.text}
               </p>
             ))}
           </span>
@@ -113,7 +138,7 @@ function App() {
           <span>
             {value.map((value, index) =>(
               <p key={index}>
-                {index}: {value.text}
+                ({index}) {value.text}
               </p>
             ))}
           </span>
@@ -122,7 +147,7 @@ function App() {
           <span>
             {object.map((object, index) =>(
               <p key={index}>
-                {index}: {object.text}
+                ({index}) {object.text}
               </p>
             ))}
           </span>
